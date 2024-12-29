a74fb41f40aa5e9a3261573fd977e23c2870bdc1
Lisa <pilipchchlisa@gmail.com>
2024-12-29 16:15:56 +0300

    adding project

---
+++ src/main/java/com/example/spring5_jokes_app_v2/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package com.example.spring5_jokes_app_v2.controllers;
+
+import com.example.spring5_jokes_app_v2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "jokes/index";
+    }
+}
---
+++ src/main/java/com/example/spring5_jokes_app_v2/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.spring5_jokes_app_v2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
